ed9c5cc7c276a00d490999789eb906894405c3b4
a028977 <a028977@progressive.com>
2023-06-12 12:29:06 -0400

    before lunch Monday

---
+++ backend/BusinessSolution/BusinessApi/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// everything above here is configuring the "guts" of teh API
+var app = builder.Build();
+//everything after here is sertting up the Request/Response pipeline
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/clock", () =>
+{
+    var response = new GetClockResponse(true, null);
+
+    return Results.Ok(response);
+});
+
+// Start the Web Server (Kestrel)
+app.Run();
+
+public record GetClockResponse(bool IsOpen, DateTime? NextOpenTime);
---
+++ backend/business-api.md
@@ -0,0 +1,25 @@
+# Business API
+
+* Introduce APIs
+    * In Particular, "Minimal APIs"
+* Developer Testing
+    * Testing from the "Outside In"
+* This will be updated later in some discussion about CI/CD
+
+## /clock
+
+GET /clock
+
+```json
+{
+    "open": true
+    "nextOpenTime": null
+}
+```
+
+```json
+{
+    "open": false
+    "nextOpenTime": ""null""
+}
+```
