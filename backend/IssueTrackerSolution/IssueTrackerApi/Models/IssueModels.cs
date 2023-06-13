1150544cbb99f6d698aa2eff83d71868398e6a6d
a028977 <a028977@progressive.com>
2023-06-13 16:34:17 -0400

    End of day 2

---
+++ backend/IssueTrackerSolution/IssueTrackerApi/Models/IssueModels.cs
@@ -0,0 +1,18 @@
+﻿namespace IssueTrackerApi.Models;
+
+public record IssueCreatedRequest
+{
+    public string From { get; init; } = String.Empty;
+    public string Issue { get; init; } = String.Empty;
+
+}
+
+public record IssueCreatedResponse
+{
+    public Guid Id { get; init; } = Guid.Empty;
+    public string From { get; init; } = String.Empty;
+    public string Issue { get; init; } = String.Empty;
+    public DateTimeOffset CreatedAt { get; init; }
+    public DateTimeOffset? ClosedAt { get; init; }
+
+}
